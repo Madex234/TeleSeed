a2654af16fea43105f04448bfaf97257012f892d
Madex234 <madex234@gmail.com>
2016-05-13 03:09:05 +0430

    Update Command_Lock.lua

--- plugins/Command_Lock.lua
+++ plugins/Command_Lock.lua
@@ -1,24 +1,29 @@
-local function run(msg)
-
+local function run(msg, matches)
+if msg.to.type == 'channel' then
+    if is_momod(msg) then
+        return
+    end
     local data = load_data(_config.moderation.data)
-
-     if data[tostring(msg.to.id)]['settings']['lock_cmd'] == 'yes' then
-
-
-if msg.to.type == 'channel' and not is_momod(msg) then
-	delete_msg(msg.id,ok_cb,false)
-	else
-	kick_user(msg.from.id, msg.to.id)
-
-        return 
-      end
-   end
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_cmd'] then
+                lock_cmd = data[tostring(msg.to.id)]['settings']['lock_cmd']
+            end
+        end
+    end
+    local channel = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_at == "yes" then
+		delete_msg(msg.id, ok_cb, true)
+    end
 end
-
-return {patterns = {
-      "id",
-      "help",
-	  "ban",
-	  "kick",
-	  "start",
-}, run = run}
+ end
+patterns = {
+    "id",
+    "help",
+    "ban",
+    "kick",
+    "start"
+  },
+  run = run
+}
