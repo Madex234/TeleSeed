7d712bb7b7b98b4083bd703ffe3117feeffaa0df
Madex234 <madex234@gmail.com>
2016-05-23 04:31:19 +0430

    Create Backup.lua

---
+++ plugins/Backup.lua
@@ -0,0 +1,13 @@
+local function run(msg, matches)
+if matches[1] == 'backup' and is_sudo(msg) then
+    local cmd = io.popen('sudo tar -cpf TeleSeed.tar *')
+      cmd:read('*all')
+      cmd:close()
+      send_document(msg.from.id, './TeleSeed.tar')
+      end
+return {
+patterns = {
+“^[!#/]backup$",
+},
+run = run
+}
