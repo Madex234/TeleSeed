a1143341f841e97a0d2d20ec84389434a4a0d908
Madex234 <madex234@gmail.com>
2016-05-23 04:47:56 +0430

    Update Backup.lua

--- plugins/Backup.lua
+++ plugins/Backup.lua
@@ -3,7 +3,7 @@ if matches[1] == 'backup' and is_sudo(msg) then
     local cmd = io.popen('sudo tar -cpf TeleSeed.tar *')
       cmd:read('*all')
       cmd:close()
-      send_document(msg.from.id, './TeleSeed.tar')
+      send_document(msg.from.id, 'TeleSeed.tar')
   end
   end
 return {
